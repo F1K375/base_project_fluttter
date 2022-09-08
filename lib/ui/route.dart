07fa25a776c91c6d52f810c11d75c0769113aa95
kharozim <kharozim@mhs.pelitabangsa.ac.id>
2022-09-08 10:46:39 +0700

    flutter launcher icoin

--- lib/ui/route.dart
+++ lib/ui/route.dart
@@ -1,9 +1,12 @@
+import 'package:base_project_flutter/ui/home_page.dart';
 import 'package:get/get_navigation/src/routes/get_route.dart';
 
 class AppRoute {
   const AppRoute._();
 
-  static final List<GetPage<dynamic>> routeList = [];
+  static final List<GetPage<dynamic>> routeList = [
+    GetPage(name: AppRoute.init, page: () => const HomePage()),
+  ];
 
   static const init = '/';
 }
